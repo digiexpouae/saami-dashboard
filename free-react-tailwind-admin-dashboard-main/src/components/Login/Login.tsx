8c14b702f62527b78312761b5f0d3b1008b1321e
ramsha1993 <rimshasaleem1993@gmail.com>
2024-12-25 13:06:18 +0500

    login page

--- free-react-tailwind-admin-dashboard-main/src/components/Login/Login.tsx
+++ free-react-tailwind-admin-dashboard-main/src/components/Login/Login.tsx
@@ -7,12 +7,12 @@ interface Props {
 const Login: React.FC<Props> = ({ }) => {
   return (
     <div className='flex justify-center items-center w-full h-screen '>
-      <div className='bg-slate-800	 w-2/5 h-2/4 rounded-lg flex items-center justify-center flex-col	gap-10'>
+      <div className='bg-slate-800	 w-2/5 h-96 rounded-lg flex items-center justify-center flex-col	gap-10'>
       <h1 className='text-5xl	font-semibold text-white'>Login</h1>
       <form action="">
 <div className='flex flex-col gap-5'>
         <div ><input className='w-80	 px-5  py-5 focus:outline-none rounded' type="text" placeholder='Enter your name' /></div>
-        <div><input className='w-80	 px-5 py-5 focus:outline-none rounded' type="text" placeholder='Enter your name' /></div>
+        <div><input className='w-80	 px-5 py-5 focus:outline-none rounded' type="text" placeholder='Enter your password' /></div>
     <button className='px-36 py-5 text-white	bg-black rounded'>Login</button></div>
         </form></div>
 
